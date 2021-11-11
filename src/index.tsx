e68fd3eb41f130ffcac18f3429325ae6753acf35
Bruno Farias <bruno_fariasd@hotmail.com>
2021-11-11 10:46:38 -0300

    add the last changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,86 @@
-import React from 'react';
-import {Animated} from 'react-native';
+import React, {useState} from 'react';
+import {Animated, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 
-import {Container, ButtonContainer, ButtonText} from './styles';
+import {
+  Container,
+  ButtonContainer,
+  ButtonText,
+  ContentButton,
+  ContainerAnimation,
+  styles,
+} from './styles';
+
+interface AnimationObject {
+  animation: Animated.Value;
+  opacity: Animated.Value;
+}
 
 const App: React.FC = () => {
-  let state = {
+  const stateAnimation: AnimationObject = {
     animation: new Animated.Value(0),
     opacity: new Animated.Value(1),
   };
+
+  const handlePressButtonAnimation = () => {
+    stateAnimation.animation.setValue(0);
+    stateAnimation.opacity.setValue(1);
+
+    Animated.timing(stateAnimation.animation, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: false,
+    }).start(({finished}) => {
+      if (finished) {
+        Animated.timing(stateAnimation.opacity, {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: false,
+        }).start();
+      }
+    });
+  };
+
+  const progressInterpolate = stateAnimation.animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0%', '100%'],
+    extrapolate: 'clamp',
+  });
+
+  const colorInterpolate = stateAnimation.animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgb(71,255,99)', 'rgb(99,71,255)'],
+  });
+
+  const progressStyle = {
+    width: progressInterpolate,
+    bottom: 0,
+
+    //Descendo de baixo pra cima
+    // height: progressInterpolate,
+    // right: 0,
+
+    //Pequena linha no rodapé do botão
+    // top: null,
+    // bottom: 0,
+    // width: progressInterpolate,
+    // height: 5,
+
+    opacity: stateAnimation.opacity,
+    backgroundColor: colorInterpolate,
+  };
+
   return (
     <Container>
-      <ButtonContainer>
-        <Container>
-          <ButtonText>Get it!</ButtonText>
-        </Container>
-      </ButtonContainer>
+      <ContentButton onPress={handlePressButtonAnimation}>
+        <ButtonContainer>
+          <ContainerAnimation style={StyleSheet.absoluteFill}>
+            <Animated.View
+              style={[styles.progress, styles.opacityBackground, progressStyle]}
+            />
+          </ContainerAnimation>
+          <ButtonText>APERTE</ButtonText>
+        </ButtonContainer>
+      </ContentButton>
     </Container>
   );
 };
--- src/styles.ts
+++ src/styles.ts
@@ -1,47 +1,40 @@
-import { Pressable, TouchableWithoutFeedback } from 'react-native';
+import {StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import styled from 'styled-components/native';
+
+export const styles = StyleSheet.create({
+  progress: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+  opacityBackground: {
+    //backgroundColor: 'rgba(255,255,255,.5)',
+  },
+});
 
 export const Container = styled.View`
   flex: 1;
-  background: #ffffff;
   justify-content: center;
   align-items: center;
-
+  background: #ffffff;
   padding: 0 20px;
 `;
 
-export const ContainerButton = styled.View`
-  justify-content: center;
-  flex-direction: row;
-  width: 100%;
-`;
+export const ContentButton = styled(TouchableWithoutFeedback)``;
 
-export const ButtonContainer = styled(TouchableWithoutFeedback)`
+export const ButtonContainer = styled.View`
   justify-content: center;
   align-items: center;
-  width: 120px;
-  height: 40px;
-  margin-right: 10px;
-
-  background: #fc03f8;
+  background: #e6537d;
+  overflow: hidden;
+  width: 100%;
+  height: 70px;
 `;
 
 export const ButtonText = styled.Text`
-  font-size: 18px;
-  font-family: 'RobotoSlab-Regular';
-  color: #ffffff;
+  font-size: 36px;
+  color: #fff;
+  background-color: transparent;
 `;
 
-export const ContainerValue = styled.View`
-  width: 180px;
-  height: 120px;
-  justify-content: center;
-  align-items: center;
-
-  margin-bottom: 10px;
-`;
-
-export const ContainerValueText = styled.Text`
-  font-size: 100px;
-  color: #000000;
-`;
+export const ContainerAnimation = styled.View``;
